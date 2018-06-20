ff1dc5479f2d631ba0bfb24977c0dd7c2fac2b56
Kasper Plougmann <kasper@squazz.dk>
2018-06-20 23:24:49 +0200

    Added default data

--- Dashboard/Dashboard/Data/DbInitializer.cs
+++ Dashboard/Dashboard/Data/DbInitializer.cs
@@ -1,5 +1,8 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using Dashboard.Models;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Dashboard.Data
 {
@@ -8,6 +11,28 @@ namespace Dashboard.Data
         public static void Initialize(ApplicationDbContext context)
         {
             context.Database.EnsureCreated();
+
+            if (context.Customers.Any())
+            {
+                return; // Database has been seeded
+            }
+
+            var customers = new List<Customer>
+            {
+                new Customer()
+                {
+                    Id = 1,
+                    CreateDate = DateTime.Now,
+                    Name = "Tapemasters Inc."
+                },
+                new Customer()
+                {
+                    Id = 2,
+                    CreateDate = DateTime.Now,
+                    Name = "Colibo A/S."
+                },
+            };
+            context.AddRange(customers);
 
             context.SaveChanges();
         }
