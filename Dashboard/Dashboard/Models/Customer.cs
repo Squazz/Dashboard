79413f92a4aaf8c74133aab44338d1ba1ec2e827
Kasper Plougmann <kasper@squazz.dk>
2018-11-13 23:10:23 +0100

    Added validation

--- Dashboard/Dashboard/Models/Customer.cs
+++ Dashboard/Dashboard/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dashboard.Models
 {
@@ -7,12 +8,14 @@ namespace Dashboard.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string VATNumber { get; set; }
         public string Address { get; set; }
         public string Att { get; set; }
         public string City { get; set; }
         public string Zip { get; set; }
+        [Phone]
         public string Phone { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime? DeleteDate { get; set; }
