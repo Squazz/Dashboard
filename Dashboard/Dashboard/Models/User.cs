9cb2a4490489e4d3fed2b4d66f5968488ac980b8
Kasper Plougmann <Github@squazz.dk>
2018-12-17 19:59:10 +0100

    Finished basics for creating a user

--- Dashboard/Dashboard/Models/User.cs
+++ Dashboard/Dashboard/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Dashboard.Models
@@ -6,11 +7,15 @@ namespace Dashboard.Models
     // Add profile data for application users by adding properties to the User class
     public class User : IdentityUser
     {
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool RecieveStatusEmails { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime? DeleteDate { get; set; }
+
+        [Required]
+        public override string Email { get; set; }
 
         public string FullName => FirstName + ", " + LastName;
 
